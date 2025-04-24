97be018e7af80b6a892e955bbd0c39c04628a2e5
”Viktor <viktorgezz@mail.ru>
2025-04-24 12:07:41 +0300

    Added a check for non-receipt of company metrics data from the database

--- definition-of-anomaly/src/main/java/ru/viktorgezz/definition_of_anomaly/service/impl/CandleAnomalousServiceImpl.java
+++ definition-of-anomaly/src/main/java/ru/viktorgezz/definition_of_anomaly/service/impl/CandleAnomalousServiceImpl.java
@@ -16,6 +16,7 @@ public class CandleAnomalousServiceImpl implements CandleAnomalousService {
     private static final Logger log = LoggerFactory.getLogger(CandleAnomalousServiceImpl.class);
 
     private static final String ANOMALOUS_VOLUME_DETECTED = "Найден аномальный объем!!! figi: {}, volume: {}, time: {}";
+    private static final String COMPANY_NOT_FOUND = "Компания не была найдена в бд figi: {}, volume: {}, time: {}";
 
     private final MetricDao metricDao;
     private final CompanyDao companyDao;
@@ -40,6 +41,10 @@ public class CandleAnomalousServiceImpl implements CandleAnomalousService {
 
     private boolean isAnomaly(CandleMessage candle) {
         final long idCompany = companyDao.getIdCompanyByFigi(candle.getFigi());
+        if (idCompany == 0) {
+            log.error(COMPANY_NOT_FOUND, candle.getFigi(), candle.getVolume(), candle.getTime());
+            return false;
+        }
         return metricDao
                 .calculateCurrentZScore(candle.getVolume(), idCompany)
                 .compareTo(metricDao.getZScoreFromTable(idCompany))
